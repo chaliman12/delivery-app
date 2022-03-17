c40cefdb847a89cc1098b62d56851d9949503842
chaliman12 <chalchisatamiru10@gmail.com>
2022-03-17 19:32:22 +0300

    All are modified
    the new laravel app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="{{ url('CSS/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('CSS/style.css') }}">
   </head>
   <body>
     <nav>
